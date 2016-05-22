35a0e9f642036fc1805191e98b134b6bebedbd4b
Iluvatar <wfoquendop@unal.edu.co>
2016-05-22 05:26:16 -0500

    Improved lmgc90 script and tested with some deposition gravity

--- scripts/lmgc90.sh
+++ scripts/lmgc90.sh
@@ -1,11 +1,25 @@
+echo "EXECUTING : lmgc90.sh"
+
 echo "Building LMGC90 ..."
 BDIR=~/PACKAGES/LMGC90
-mkdir -p ${BDIR} &>/dev/null
+if [ ! -d ${BDIR} ]; then
+    mkdir -p ${BDIR}
+fi
 cd ${BDIR}
+
+#wget -c http://www.vtk.org/files/release/6.3/vtkpython-6.3.0-Linux-64bit.tar.gz 
+
+echo vagrant | sudo apt-get install -y gmsh python-vtk gmsh libgmsh-dev 
+conda install -y vtk
+pip install -y pygmsh 
+
 if [ ! -d lmgc90_user ]; then
     export GIT_SSL_NO_VERIFY=True
     git clone https://git-xen.lmgc.univ-montp2.fr/lmgc90/lmgc90_user.git
 fi
+cd lmgc90_user
+git pull origin master
+cd ..
 if [ ! -d build ]; then
     mkdir build 
     cd build
@@ -21,9 +35,9 @@ echo "Adding lmgc90 stuff to bashrc ..."
 STATUS=$(grep lmgc90_user ~/.bashrc | grep -v grep)
 if [ "" == "$STATUS" ]; then
     if [ -z ${PYTHONPATH} ]; then
-	export PYTHONPATH=${BDIR}/build
+	echo export PYTHONPATH=${BDIR}/build >> ~/.bashrc
     else
-	export PYTHONPATH=${PYTHONPATH}:${BDIR}/build
+	echo export PYTHONPATH=${PYTHONPATH}:${BDIR}/build >> ~/.bashrc
     fi
 fi
 echo "Done."
