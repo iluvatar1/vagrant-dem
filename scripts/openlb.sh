115d51622ec191b057df54fa4db3a55864509f33
Iluvatar <wfoquendop@unal.edu.co>
2016-05-22 15:45:03 -0500

    Added openlb (lattice Boltzmann)

---
+++ scripts/openlb.sh
@@ -0,0 +1,25 @@
+echo "Executing openlb.sh"
+
+BDIR=~/PACKAGES/OPENLB
+if [ ! -d ${BDIR} ]; then
+    mkdir ${BDIR}
+fi
+
+cd ${BDIR}
+
+PNAME=olb-1.0r0.tgz
+BNAME=${PNAME%.tgz}
+if [ ! -f $PNAME ]; then
+    wgetc -c http://www.optilb.com/openlb/wp-content/uploads/2016/03/$PNAME
+fi
+if [ ! -d $BNAME ]; then
+    tar xf $PNAME
+fi
+
+cd $BNAME
+echo "Compiling openlb lib ..."
+make -j 2
+echo "Compiling samples"
+make -j 2 samples
+
+echo "Done."
