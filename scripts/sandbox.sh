fc978f54dc56ab3f7a22855fe4fb6924c355cadb
Iluvatar <wfoquendop@unal.edu.co>
2016-05-22 05:49:43 -0500

    Improved and tested sandbox script

--- scripts/sandbox.sh
+++ scripts/sandbox.sh
@@ -1,18 +1,36 @@
+echo "EXECUTING : sandbox.sh"
+
+if [ ! -d ~/bin ]; then
+    mkdir -p ~/bin
+fi
+
 echo "Installing SandBox ..."
 BDIR=/home/vagrant/PACKAGES/SANDBOX
 if [ ! -d ${BDIR} ]; then
     mkdir -p ${BDIR}
 fi
 cd ${BDIR}
-if [ ! -d sandbox ]; then
-    git clone https://bitbucket.org/iluvatar/sandbox
-fi
-if [ ! -f /usr/local/bin/SandBox ]; then
+if [ ! -f ~/bin/SandBox ]; then
+    if [ ! -d sandbox ]; then
+	git clone https://bitbucket.org/iluvatar/sandbox
+    fi
     cd sandbox
+    git checkout .
+    git pull
     make FC=gfortran
-    ln -sf $PWD/BuildSample/build_SandBox /usr/local/bin/
-    ln -sf $PWD/POSTPRO/postpro_SandBox /usr/local/bin/
-    ln -sf $PWD/SANDBOX/SandBox /usr/local/bin/
+    ln -sf $PWD/BuildSample/build_SandBox ~/bin/
+    ln -sf $PWD/POSTPRO/postpro_SandBox ~/bin/
+    ln -sf $PWD/SANDBOX/SandBox ~/bin/
+    cd ..
 fi
+if [ ! -f ~/bin/draw_SandBox_paraview.py ]; then
+    cd sandbox
+    chmod a+x $PWD/ADDON/draw_SandBox_paraview.py
+    ln -sf $PWD/ADDON/draw_SandBox_paraview.py ~/bin/
+    cd ..
+fi
+
+echo "Installing PyEVTK"
+pip install PyEVTK
 
 echo "Done."
